b06a185b1ad1e98a8d804e025148b88bd2d97d36
mathewfonteyne <111210884+mathewfonteyne@users.noreply.github.com>
2023-08-29 23:00:16 -0400

    developer portfolio first push

---
+++ developer-portfolio/src/App.jsx
@@ -0,0 +1,8 @@
+import "./App.scss";
+import HomePage from "./components/HomePage";
+
+function App() {
+  return <div className="App">{/* <HomePage /> */}</div>;
+}
+
+export default App;
---
+++ developer-portfolio/src/components/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "./Navbar";
+import { Card } from "reactstrap";
+
+export default function HomePage() {
+  return (
+    <div>
+      <Card>Home Page</Card>
+      <Navbar />
+    </div>
+  );
+}
