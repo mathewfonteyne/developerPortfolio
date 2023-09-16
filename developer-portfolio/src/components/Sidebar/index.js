c3a19b61e9d0fbc385b9ba90515433b9677d1261
mathewfonteyne <111210884+mathewfonteyne@users.noreply.github.com>
2023-09-16 12:50:03 -0400

    Added links for conact and about along with styling. git push git status

--- developer-portfolio/src/components/Sidebar/index.js
+++ developer-portfolio/src/components/Sidebar/index.js
@@ -4,7 +4,7 @@ import { Link, NavLink } from "react-router-dom";
 import LogoS from "../../assets/images/logo-s.png";
 // import LogoSubtitle from "../../assets/images/logos/logo-sub.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHome } from "@fortawesome/free-solid-svg-icons";
+import { faHome, faEnvelope, faUser } from "@fortawesome/free-solid-svg-icons";
 
 const Sidebar = () => (
   <div className="nav-bar">
@@ -15,6 +15,22 @@ const Sidebar = () => (
     <nav>
       <NavLink exact="true" activeclassname="active" to="/">
         <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+      </NavLink>
+      <NavLink
+        exact="true"
+        activeclassname="active"
+        className="about-link"
+        to="/about"
+      >
+        <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+      </NavLink>
+      <NavLink
+        exact="true"
+        activeclassname="active"
+        className="contact-link"
+        to="/contact"
+      >
+        <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
       </NavLink>
     </nav>
   </div>
