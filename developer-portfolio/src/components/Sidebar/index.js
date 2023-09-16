fa70a7ecff00376ea18a1ec0065c3120e2ff44d4
mathewfonteyne <111210884+mathewfonteyne@users.noreply.github.com>
2023-09-16 13:11:19 -0400

    Added links and logos for linkedin and github to this website page

--- developer-portfolio/src/components/Sidebar/index.js
+++ developer-portfolio/src/components/Sidebar/index.js
@@ -5,6 +5,7 @@ import LogoS from "../../assets/images/logo-s.png";
 // import LogoSubtitle from "../../assets/images/logos/logo-sub.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHome, faEnvelope, faUser } from "@fortawesome/free-solid-svg-icons";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 const Sidebar = () => (
   <div className="nav-bar">
@@ -33,6 +34,26 @@ const Sidebar = () => (
         <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
       </NavLink>
     </nav>
+    <ul>
+      <li>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://www.linkedin.com/in/mathewfonteyne/"
+        >
+          <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+        </a>
+      </li>
+      <li>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://github.com/mathewfonteyne"
+        >
+          <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+        </a>
+      </li>
+    </ul>
   </div>
 );
 
