f51aafae2932fa6748ceae3bf28ce9499f1b5ba9
mathewfonteyne <111210884+mathewfonteyne@users.noreply.github.com>
2023-09-16 12:33:11 -0400

    Created home button link with icon from fontAwaesome

--- developer-portfolio/src/components/Sidebar/index.js
+++ developer-portfolio/src/components/Sidebar/index.js
@@ -1,7 +1,10 @@
 import "./index.scss";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
+// Design an image to be used as my logo for both my home button/name and subtitle/name
 import LogoS from "../../assets/images/logo-s.png";
 // import LogoSubtitle from "../../assets/images/logos/logo-sub.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome } from "@fortawesome/free-solid-svg-icons";
 
 const Sidebar = () => (
   <div className="nav-bar">
@@ -9,6 +12,11 @@ const Sidebar = () => (
       <img src={LogoS} alt="logo" />
       {/* <img src={LogoSubtitle} alt="Mathew Fonteyne" /> */}
     </Link>
+    <nav>
+      <NavLink exact="true" activeclassname="active" to="/">
+        <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+      </NavLink>
+    </nav>
   </div>
 );
 
