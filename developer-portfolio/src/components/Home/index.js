0eb14687f037addd89af4c6500261eb2206a9102
mathewfonteyne <111210884+mathewfonteyne@users.noreply.github.com>
2023-10-17 18:28:57 -0400

    Added in a basic intro text to the home page. Need to create an image that is used for my brand/name.

--- developer-portfolio/src/components/Home/index.js
+++ developer-portfolio/src/components/Home/index.js
@@ -1,9 +1,17 @@
+import LogoS from "../../assets/images/logo-s.png";
+// this logo is placeholder for what my actual logo will be.
+
 const Home = () => {
   return (
     <div className="container home-page">
       <div className="text-zone">
         <h1>
           Hi, <br /> I'm
+          {/* Put an image here that matches my brand and introduces myself i.e logo */}
+          <img src={LogoS} alt="developer" />
+          athew
+          <br />
+          Software Developer
         </h1>
       </div>
     </div>
