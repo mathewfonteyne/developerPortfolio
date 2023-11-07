014f62d97be8921beb9aef60442d1c6c7c506769
mathewfonteyne <111210884+mathewfonteyne@users.noreply.github.com>
2023-11-07 18:53:16 -0500

    Created animation to h1 text upon screen load.

--- developer-portfolio/src/components/Home/index.js
+++ developer-portfolio/src/components/Home/index.js
@@ -4,19 +4,56 @@ import LogoS from "../../assets/images/purple_m.png";
 import { Link } from "react-router-dom";
 // this logo is placeholder for what my actual logo will be.
 import "./index.scss";
+import AnimatedLetters from "../AnimatedLetters";
 
 const Home = () => {
   const [letterClass, setLetterClass] = useState("text-animate");
+  const nameArray = ["a", "t", "h", "e", "w"];
+  const jobArray = [
+    "s",
+    "o",
+    "f",
+    "t",
+    "w",
+    "a",
+    "r",
+    "e",
+    " ",
+    "d",
+    "e",
+    "v",
+    "e",
+    "l",
+    "o",
+    "p",
+    "e",
+    "r",
+    ".",
+  ];
   return (
     <div className="container home-page">
       <div className="text-zone">
         <h1>
-          Hi, <br /> I'm
+          <span className={letterClass}>H</span>
+          <span className={`${letterClass} _12`}>i,</span>
+          <br />
+          <span className={`${letterClass} _13`}>I</span>
+          <span className={`${letterClass} _14`}>'m</span>
           {/* Put an image here that matches my brand and introduces myself i.e logo */}
           <img src={LogoS} alt="developer" />
-          athew
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={nameArray}
+            idx={15}
+          />
           <br />
-          Software Developer
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={jobArray}
+            idx={22}
+          />
+          {/* <br />
+          Software Developer */}
         </h1>
         <h2>Full-Stack Developer / Game Developer</h2>
         <Link to="/contact" className="flat-button">
